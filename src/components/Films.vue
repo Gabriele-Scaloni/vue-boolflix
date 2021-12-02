e6843eab87f38162b359928aeae47d9a5d5c3831
Gabriele-Scaloni <88436304+Gabriele-Scaloni@users.noreply.github.com>
2021-12-02 16:56:12 +0100

    add: FlagLanguage in Films

--- src/components/Films.vue
+++ src/components/Films.vue
@@ -1,35 +1,36 @@
 <template>
-
-    <div>
-        <img :src="details.poster_path" alt="film">
-        <p>{{details.title}}</p>
-        <p>{{details.original_title}}</p>
-        <p>{{details.original_language}}</p>
-        <p>{{details.vote_average}}</p>
-    </div>
-
+  <div class="filmcard">
+    <!-- <img :src="details.poster_path" alt="film" /> -->
+    <h3>Titolo : {{ details.title }}</h3>
+    <p>Titolo originale: {{ details.original_title }}</p>
+    <!-- <p>Lingua originale : {{ details.original_language }}</p> -->
+    <div><span> Lingua :<img class="flag" :src="FlagLanguage()" alt=""></span></div>
+    <p>Voto : {{ details.vote_average }}</p>
+  </div>
 </template>
 
 <script>
-
 export default {
-  name: 'Films',
+  name: "Films",
   props: {
     details: Object,
   },
-    /* const this.details.original_language = lingua; */
-methods :{
+  /* const this.details.original_language = lingua; */
+  methods: {
     FlagLanguage() {
-        if ( this.details.original_language === 'it'){
-            return "https://m.media-amazon.com/images/I/31g0+P5xoML._AC_SX466_.jpg"
-        } else if ( this.details.original_language === 'en'){
-            return "https://m.media-amazon.com/images/I/41+fJSCwMJL._AC_.jpg"
-        }
-    }
-}
-}
+      if (this.details.original_language === "it") {
+        return "https://m.media-amazon.com/images/I/31g0+P5xoML._AC_SX466_.jpg";
+      } else if (this.details.original_language === "en") {
+        return "https://m.media-amazon.com/images/I/41+fJSCwMJL._AC_.jpg";
+      }
+    },
+  },
+};
 </script>
 
 <style lang="scss">
-
+.flag {
+    width: 30px;
+    height: 20px;
+}
 </style>

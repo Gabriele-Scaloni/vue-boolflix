119c0fb11a2a249ebf1a7bb8fc6ea645d986cfa0
Gabriele-Scaloni <88436304+Gabriele-Scaloni@users.noreply.github.com>
2021-12-03 11:12:06 +0100

    add vote condition

--- src/components/Films.vue
+++ src/components/Films.vue
@@ -1,17 +1,17 @@
 <template>
-    <div class="container-cards">
-        <div class="film-card">
-            <!-- <img :src="details.poster_path" alt="film" /> -->
-            <h3>Titolo: {{ details.title }}</h3>
-            <p>Titolo originale: {{ details.original_title }}</p>
-            <!-- <p>Lingua originale : {{ details.original_language }}</p> -->
-            <div>
-                <span> Lingua:</span>
-                <img class="flag" :src="FlagLanguage()" alt="">
-            </div>
-            <p>Voto: {{ details.vote_average }}</p>
-        </div>
+  <div class="container-cards">
+    <div class="film-card">
+      <img :src="details.poster_path" alt="film" />
+      <h3>Titolo: {{ details.title }}</h3>
+      <p>Titolo originale: {{ details.original_title }}</p>
+      <p>Lingua originale : {{ details.original_language }}</p>
+      <div>
+        <span> Lingua:</span>
+        <img class="flag" :src="FlagLanguage()" alt= "" />
+      </div>
+      <p>Voto: {{ vote() }} </p>
     </div>
+  </div>
 </template>
 
 <script>
@@ -27,27 +27,47 @@ export default {
         return "https://m.media-amazon.com/images/I/31g0+P5xoML._AC_SX466_.jpg";
       } else if (this.details.original_language === "en") {
         return "https://m.media-amazon.com/images/I/41+fJSCwMJL._AC_.jpg";
+      } else {
+        return "https://www.prestashop.com/sites/default/files/styles/blog_750x320/public/blog/2019/10/banner_error_404.jpg?itok=eAS4swln"
       }
     },
-  },
+    vote(){ 
+        if ( this.details.vote_average === 0) {
+        return 0
+      } else if ( this.details.vote_average <= 2 != 0) {
+        return 1 
+      } else if ( this.details.vote_average <= 4) {
+        return 2 
+      } else if ( this.details.vote_average <= 6) {
+        return 3 
+      } else if ( this.details.vote_average <= 8) {
+        return 4 
+      } else if( this.details.vote_average <= 10) {
+        return 5 
+      }
+
+    }
+ },
 };
 </script>
 
 <style lang="scss">
 .container-cards {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: flex-start;
+  float: left;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-start;
 }
 .film-card {
-    width: 150px;
-    height: 300px;
-
-    background-color: rgb(105, 105, 105);
-    margin: 10px 5px;
+  width: 150px;
+  height: 300px;
+  float: left;
+  background-color: rgb(105, 105, 105);
+  margin: 10px 5px;
+  padding: 10px 5px;
 }
 .flag {
-    width: 30px;
-    height: 20px;
+  width: 30px;
+  height: 20px;
 }
 </style>
--- src/components/Series.vue
+++ src/components/Series.vue
@@ -1,19 +1,17 @@
 <template>
-
-    <div class="container-cards-serie">
-        <div class="serie-card">
-            <!-- <img :src="details.poster_path" alt="serie" /> -->
-            <h3>Titolo: {{ particolari.name }}</h3>
-            <p>Titolo originale: {{ particolari.original_name }}</p>
-            <!-- <p>Lingua originale : {{ particolari.original_language }}</p> -->
-            <div>
-                <span> Lingua:</span>
-                <img class="flag" :src="FlagLanguage()" alt="">
-            </div>
-            <p>Voto: {{ particolari.vote_average }}</p>
-        </div>
-    </div>
-
+  <div class="container-cards-serie">
+      <div class="serie-card">
+          <!-- <img :src="details.poster_path" alt="serie" /> -->
+          <h3>Titolo: {{ particolari.name }}</h3>
+          <p>Titolo originale: {{ particolari.original_name }}</p>
+          <!-- <p>Lingua originale : {{ particolari.original_language }}</p> -->
+          <div>
+              <span> Lingua:</span>
+              <img class="flag" :src="FlagLanguage()" alt="">
+          </div>
+          <p>Voto: {{ particolari.vote_average }}</p>
+      </div>
+  </div>
 </template>
 
 <script>
